341352da98862430c37f13d12cf35ccf88855285
Dmitriy Bartoshchak <kaskad.8888@gmail.com>
2022-06-16 11:53:33 +0300

    UPD: bump version to 1.8.0

--- cherry-x-interface-builder.php
+++ cherry-x-interface-builder.php
@@ -2,7 +2,7 @@
 /**
  * Interface Builder module
  *
- * Version: 1.7.9
+ * Version: 1.8.0
  */
 
 // If this file is called directly, abort.
@@ -42,7 +42,7 @@ if ( ! class_exists( 'CX_Interface_Builder' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $version = '1.7.9';
+		protected $version = '1.8.0';
 
 		/**
 		 * Conditions
